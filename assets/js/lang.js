ec6dfe9bdd644a07cdd6482b9543bfc9066af7f6
BarinovVlad <vladislav.barinovv@gmail.com>
2024-08-11 18:56:30 +0200

    Add files via upload

---
+++ assets/js/lang.js
@@ -0,0 +1,27 @@
+const langArr = {
+    "home": {
+        "ua": "Дiм",
+        "en":"Home",
+    },
+    "works": {
+        "ua": "Роботи",
+        "en":"Works",
+    },
+    "about": {
+        "ua": "Про нас",
+        "en":"About",
+    },
+    "contact": {
+        "ua": "Контакти",
+        "en":"Kontact",
+    },
+    "hireUs": {
+        "ua": "Напиш нам",
+        "en":"Hire us",
+    },
+    "device-notification--message": {
+        "ua": "Напиш нам, як справи чар друже привит пока дом школа и ще щось",
+        "en":"Global has so much to offer that we must request you orient your device to portrait or find a larger screen. You won't be disappointed us",
+    },
+
+}
